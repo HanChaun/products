52b93def5781c05301ad13736ab2b03e0ae7b2bc
charle <jack107t@hotmail.com>
2018-09-05 20:50:17 +0800

    add Field name

--- products.py
+++ products.py
@@ -1,12 +1,17 @@
 products = []
 while True:
-	name = input('pls input product name:')
+	name = raw_input('pls input product name:')
 	if name == 'q':
 		break
-	price = input('pls input product price:')
+	price = raw_input('pls input product price:')
 	products.append([name, price])
 print(products)
 
 for p in products:
 	print(p)
 	print(p[0], 'price is', p[0])
+
+with open('products.csv','w') as f:
+	f.write('Products,Price\n')
+	for p in products:
+		f.write(p[0] + ',' + p[1] + '\n')
